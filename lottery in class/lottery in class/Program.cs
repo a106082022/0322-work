05547f9d946ff2ece7f73ceeb09459430df1c14d
Michael <A106082022@mail.shu.edu.tw>
2018-03-22 16:11:37 +0800

    可以產生6個亂數

--- lottery in class/lottery in class/Program.cs
+++ lottery in class/lottery in class/Program.cs
@@ -12,9 +12,16 @@ namespace lottery_in_class
         {
             //產生亂數物件
             Random rand = new Random();
-            
-            //印出亂數
-            Console.WriteLine(rand.Next(50,100));
+
+            //產生6個亂數
+            int r1 = rand.Next(1, 43);
+            int r2 = rand.Next(1, 43);
+            int r3 = rand.Next(1, 43);
+            int r4 = rand.Next(1, 43);
+            int r5 = rand.Next(1, 43);
+            int r6 = rand.Next(1, 43);
+            //印出6個亂數
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", r1, r2, r3, r4, r5, r6);
 
             //暫停螢幕
             Console.ReadLine();
