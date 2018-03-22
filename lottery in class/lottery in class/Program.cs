5d75428881c1df0a7ca7402216eb65ba1b8932cf
Michael <A106082022@mail.shu.edu.tw>
2018-03-22 16:05:26 +0800

    第一次test

---
+++ lottery in class/lottery in class/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lottery_in_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //產生亂數物件
+            Random rand = new Random();
+            
+            //印出亂數
+            Console.WriteLine(rand.Next(50,100));
+
+            //暫停螢幕
+            Console.ReadLine();
+        }
+    }
+}
